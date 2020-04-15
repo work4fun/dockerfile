8a6fef561d9da1179cb65113a192759936c9fe3e
Albert <albert_p@foxmail.com>
2020-04-16 04:15:31 +0800

    chore: added tips

--- scripts/push.sh
+++ scripts/push.sh
@@ -7,4 +7,9 @@ set -o pipefail
 SCRIPTS_DIR="$(dirname "${BASH_SOURCE}")"
 source "${SCRIPTS_DIR}/common.sh"
 
+if [ -z "$(get_latest_id)" ];then
+    echo "latest build docker image not found, retry make build?"
+    exit 255
+fi
+
 $DOCKER push "$(get_latest_id)"
